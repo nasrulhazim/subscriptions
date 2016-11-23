249944ea0099950f278025392a387e45bc36dfd3
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-11-24 02:05:00 +0800

    Added Fixed Syntax Error

--- src/Providers/SubscriptionServiceProvider.php
+++ src/Providers/SubscriptionServiceProvider.php
@@ -17,15 +17,21 @@ class SubscriptionServiceProvider extends ServiceProvider
             $this->loadMigrationsFrom(dirname(dirname(__FILE__)) . '/database/migrations/');
         }
 
-        $this->loadRoutesFrom(dirname(dirname(__FILE__)) .'/routes/web.php');
+        $this->loadRoutesFrom(dirname(dirname(__FILE__)) . '/routes/web.php');
 
         $this->loadViewsFrom(dirname(dirname(__FILE__)) . '/resources/views', 'subscriptions');
 
         $this->publishes([
             dirname(dirname(__FILE__)) . '/config/subscription.php' => config_path('subscription.php'),
-            dirname(dirname(__FILE__)) . '/resources/views', 'subscriptions') => resource_path('views/vendor/subscriptions')),
-            dirname(dirname(__FILE__)) .'/public' => public_path(),
-        ]);
+        ], 'config');
+
+        $this->publishes([
+            dirname(dirname(__FILE__)) . '/resources/views' => resource_path('views/vendor/subscriptions'),
+        ], 'resources');
+
+        $this->publishes([
+            dirname(dirname(__FILE__)) . '/public' => public_path(),
+        ], 'public');
     }
 
     /**
