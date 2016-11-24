8b12df5ebe11a7a4aaff1a810a396373505ea1d3
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-11-25 05:13:51 +0800

    Add missing subscription model

---
+++ src/Subscription.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace CleaniqueCoders;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subscription extends Model
+{
+    protected $fillable = [
+        'name', 'label',
+        'status', 'type', 'duration',
+        'price', 'details', 'meta',
+    ];
+
+    protected $casts = [
+        'meta' => 'array',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('CleaniqueCoders\User');
+    }
+}
