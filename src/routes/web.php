4880332dfc6b47aa57243f43afd756dd86bc27a1
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-11-26 01:15:46 +0800

    remove routes, see README.md

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,13 +1,13 @@
 <?php
 // Subscription routes
-Route::get('subscribe/{id}', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@subscribe')->name('subscriptions.subscribe');
-Route::get('unsubscribed', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@unsubscribed')->name('subscriptions.unsubscribed');
+// Route::get('subscribe/{id}', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@subscribe')->name('subscriptions.subscribe');
+// Route::get('unsubscribed', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@unsubscribed')->name('subscriptions.unsubscribed');
 
-// must be logged in
-Route::group(['middleware' => ['auth']], function () {
-    Route::get('expired', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@expired')->name('subscriptions.expired');
-});
+// // must be logged in
+// Route::group(['middleware' => ['auth']], function () {
+//     Route::get('expired', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@expired')->name('subscriptions.expired');
+// });
 
-Route::group(['middleware' => ['auth', 'subscription']], function () {
-    Route::resource('subscriptions', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController');
-});
+// Route::group(['middleware' => ['auth', 'subscription']], function () {
+//     Route::resource('subscriptions', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController');
+// });
