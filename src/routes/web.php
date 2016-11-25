aa11b271fba82b03783b793ef04fb9aa92f0a2aa
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-11-26 00:53:58 +0800

    update route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,11 +1,11 @@
 <?php
 // Subscription routes
 Route::get('subscribe/{id}', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@subscribe')->name('subscriptions.subscribe');
+Route::get('unsubscribed', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@unsubscribed')->name('subscriptions.unsubscribed');
 
 // must be logged in
 Route::group(['middleware' => ['auth']], function () {
     Route::get('expired', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@expired')->name('subscriptions.expired');
-    Route::get('unsubscribed', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@unsubscribed')->name('subscriptions.unsubscribed');
 });
 
 Route::group(['middleware' => ['auth', 'subscription']], function () {
