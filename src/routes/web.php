c470aa2332aafc9e5ad12271fb0c9ff13069dfa5
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-11-25 23:48:14 +0800

    update route

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,15 +1,14 @@
 <?php
+// Subscription routes
+Route::get('subscribe/{id}', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@subscribe')->name('subscriptions.subscribe');
 
-// allow public user to see, but require registration upon subscribe
-Route::get('subscribe', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@subscribe')->name('subscriptions.subscribe');
-
-// must be registered user
+// must be logged in
 Route::group(['middleware' => ['auth']], function () {
     Route::get('expired', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@expired')->name('subscriptions.expired');
     Route::get('unsubscribed', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController@unsubscribed')->name('subscriptions.unsubscribed');
 });
 
-// to access to internal subscription modules, can enhance to allow only particular roles
 Route::group(['middleware' => ['auth', 'subscription']], function () {
+    Route::get('/home', 'HomeController@index');
     Route::resource('subscriptions', '\CleaniqueCoders\Subscription\Http\Controllers\SubscriptionController');
 });
